e31437733c45ac4396865ada9530d9d292d335af
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-09-06 13:53:54 +0200

    Update M2.m

--- Computational Mechanics/MOD2/M2.m
+++ Computational Mechanics/MOD2/M2.m
@@ -1,30 +1,67 @@
 %==============================================================================
 % MOD2 4MC00 / Jelle Langedijk / TU/e
-% V1.0 (C) 2020 Jelle Langedijk, all rights reserved
+% V1.1 (C) 2020 Jelle Langedijk, all rights reserved
 % https://github.com/JelleLa/templates/tree/master/MATLAB
 %==============================================================================
 
 %% PREREQUISITIES
-clear all; close all; clear vars;
+clear all; close all; clear vars; clc;
+
+
+ 
+
+%% VERTICAL LOAD
+% f  = 10^(2)*e^(-1000*(x-(1)/(2))^(2))
+
+h = 2^(-3);             %Step Size
+L = 1;                  %Length of xi (See BC's: from 0 to 1)
+xi = [0:h:L];
+fi = 10.^(2)*exp(-1000*(xi-(1)/(2)).^(2));
+b = fi';
 
 %% MATRIX FILLER
 
-N = 8;
+N = length(xi) ;
 
 
-Ah = zeros(N);
+A = zeros(N);
 
 for i = 1:N
-    Ah(i,i) = -2;
+    A(i,i) = -2;
 end   
 
 for j = 1:N-1
-    Ah(j,j+1) = +1;
+    A(j,j+1) = +1;
 end  
 
 for k = 2:N
-    Ah(k,k-1) = +1;
-end  
+    A(k,k-1) = +1;
+end 
+    
+%% SOLVE
+Ah = (-1)/(h^(2)).*A ;  %Ah Matrix
 
-%% 
+U = Ah\b;
+
+%% MIDPOINT DISPLACEMENT
+
+mid = ceil(length(xi)/2);
+disp = b(mid) - U(mid);
+
+
+
+%% PLOTS
+
+figure(1)
+plot(xi , U) ;
+xlabel("x_i") ;
+ylabel("u^h_i") ;
+title("MODULE 2 EX 3a") ;
+
+figure(2)
+plot(xi , fi) ;
+xlabel("x_i") ;
+ylabel("f_i") ;
+title("MODULE 2 EX 3b") ;
+
 
